0fc877bbb11a4f650c9d9cbeee06de34bea92ef4
Joseph765 <joseph.chauvin102@gmail.com>
2020-10-16 20:48:15 -0500

    Changed font family and weight

--- client/src/global.styles.js
+++ client/src/global.styles.js
@@ -2,6 +2,12 @@ import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
+
+  * {
+    font-family: Arial;
+    font-weight: 300;
+  }
+
   body {
     margin: 0;
     padding: 0;
@@ -9,4 +15,5 @@ export const GlobalStyle = createGlobalStyle`
     background-image: url('./img/starry.gif');
     color: white;
   }
+
 `;
