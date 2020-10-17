dee49bd1a434a24e8978a37970d764c8d49b75fc
Joseph765 <joseph.chauvin102@gmail.com>
2020-10-16 20:36:59 -0500

    Changed to hash router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Home } from './home';
 import { Music } from './music';
