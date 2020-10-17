ac9d9ec3285a89f8271a47033b7c13bb35d34100
Joseph765 <joseph.chauvin102@gmail.com>
2020-10-17 17:44:00 -0500

    added a new desktop landing page design, but need to modify for mobile

--- client/src/home.js
+++ client/src/home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Purple, Magenta, Maroon, Indigo, Title, HomeWrapper } from './home.styles';
+import { Purple, Magenta, Maroon, Indigo, Title, HomeWrapper, SubTitle, Profile, ProfileImg } from './home.styles';
 import { GlobalStyle } from './global.styles';
 import { Header } from './header';
 import { News } from './news';
@@ -17,7 +17,13 @@ export const Home = () => {
       <GlobalStyle />
       <Header />
       <HomeWrapper>
-        <Title>Martin Moonlite</Title>
+        <Profile>
+          <div id="img-subtitle">
+            <Title>Martin Moonlite &#x263E;</Title>
+            <SubTitle>Music Producer | Songwriter</SubTitle>
+          </div>
+          <ProfileImg src="./img/profile.jpg" />
+        </Profile>
         <News />
         <Blog />
       </HomeWrapper>
--- client/src/home.styles.js
+++ client/src/home.styles.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+const media = {
+  desktop: '@media(min-width: 1000px)'
+}
+
 export const HomeWrapper = styled.div `
   position: absolute;
   z-index: -1;
@@ -7,10 +11,42 @@ export const HomeWrapper = styled.div `
   top: 10%;
 `;
 
+export const Profile = styled.div`
+  margin-bottom: 10%;
+  #img-subtitle {
+    margin-top: 2%;
+  }
+`;
+
 export const Title = styled.h1`
-  font-size: 45px;
+  margin-top: 0;
   text-align: center;
+  font-weight: 700;
+  font-size: 70px;
   letter-spacing: 2px;
+  align-self: center;
+  text-shadow: 8px 8px 3px #541363;
+`;
+
+export const ProfileImg = styled.img`
+  /* border: 5px solid #541463; */
+  width: 100%;
+  display: block;
+  object-fit: cover;
+  /* opacity: 0.4; */
+  margin: auto;
+  ${media.desktop} {
+    width: auto;
+  }
+`;
+
+export const SubTitle = styled.h1`
+  text-align: center;
+  font-weight: 500;
+  font-size: 30px;
+  letter-spacing: 2px;
+  align-self: center;
   margin-bottom: 5%;
+  text-shadow: 8px 8px 5px #541363;
 `;
 
