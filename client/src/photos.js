e433ab177e5fd5e0d4660de50a1cf5dc32c85783
Joseph765 <joseph.chauvin102@gmail.com>
2020-10-19 12:07:09 -0500

    added photos module for desktop

--- client/src/photos.js
+++ client/src/photos.js
@@ -1,19 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { GlobalStyle } from './global.styles';
 import { Header } from './header';
-import { PhotosTitle } from './photos.styles';
+import { PhotosTitle, ImageCarousel, AllPhotos } from './photos.styles';
 
 export const Photos = () => {
+
+  const [images, setImages] = useState(["./img/profile.jpg", "./img/profile2.jpg", "./img/ki.jpg", "./img/mixinsoda.jpg", "./img/keepquiet.jpg", "./img/mixinsoda2.jpg", "./img/cat.jpg"]);
+  const [currentImage, setCurrentImage] = useState("./img/profile.jpg");
+  const [index, setIndex] = useState(0);
 
   useEffect(() => {
 
   }, []);
+
+  function changePhoto(direction) {
+    if (direction === "right") {
+      if (index === images.length - 1) {
+        setIndex(0);
+      } else {
+        setIndex(index + 1);
+      }
+    } else {
+      if (index === 0) {
+        setIndex(images.length - 1);
+      } else {
+        setIndex(index - 1);
+      }
+    }
+    setCurrentImage(images[index]);
+  }
+
+  const allPhotos = images.map(item => {
+    return <img src={item} />
+  });
 
   return (
     <>
       <Header />
       <GlobalStyle />
       <PhotosTitle>Photos</PhotosTitle>
+      <ImageCarousel>
+        <a className="arrow-left" onClick={() => changePhoto("left")}>&#10094;</a>
+        <a className="arrow-right" onClick={() => changePhoto("right")} >&#10095;</a>
+        <img src={currentImage} />
+      </ImageCarousel>
+      <AllPhotos>
+      <PhotosTitle>All Photos</PhotosTitle>
+        {allPhotos}
+      </AllPhotos>
     </>
   );
 }
--- client/src/photos.styles.js
+++ client/src/photos.styles.js
@@ -7,3 +7,35 @@ export const PhotosTitle = styled.h1 `
   margin-bottom: 5%;
   margin-top: 10%;
 `;
+
+export const ImageCarousel = styled.div `
+  width: 80%;
+  margin: auto;
+  position: relative;
+  margin-bottom: 10%;
+  .arrow-left, .arrow-right {
+    font-size: 30px;
+    position: absolute;
+    top: 50%;
+    cursor: pointer;
+    user-select: none;
+  }
+  .arrow-right {
+    right: 0;
+  }
+
+  img {
+    height: 500px;
+    user-select: none;
+    display: block;
+    margin: auto;
+  }
+`;
+
+export const AllPhotos = styled.div `
+  width: 80%;
+  margin: auto;
+  img {
+    height: 300px;
+  }
+`;
